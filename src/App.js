5375f8908b70c6f829b4d286212e806338c66582
Aharnish Solanki <ahar2899@gmail.com>
2023-12-23 05:54:52 -0400

    major parts completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Aboutjs from './components/about/aboutjs.jsx';
 import Skills from './components/skills/Skillsjs.jsx';
 import Servicesjs from './components/services/Servicesjs.jsx';
 import Qualification from './components/qualification/Qualificationjs.jsx';
+import Contact from './components/contact/Contactjs.jsx';
+import Footer from './components/footer/Footerjs.jsx';
+import ScrollUp from './components/scrollup/ScrollUpjs.jsx';
 
 function App() {
   return (
@@ -17,7 +20,10 @@ function App() {
     <Skills />
     <Servicesjs />
     <Qualification />
+    <Contact />
     </main>
+    <Footer />
+    <ScrollUp />
   </>
   );
 }
