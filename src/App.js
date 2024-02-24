aab4d38ecdff1f2ce07c735a34629aad992b792e
Aharnish Solanki <ahar2899@gmail.com>
2024-02-24 18:11:51 -0400

    removed service.js warning

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/Headerjs.jsx';
 import Home from './components/home/Homejs';
 import Aboutjs from './components/about/aboutjs.jsx';
 import Skills from './components/skills/Skillsjs.jsx';
-import Servicesjs from './components/services/Servicesjs.jsx';
+// import Servicesjs from './components/services/Servicesjs.jsx';
 import Qualification from './components/qualification/Qualificationjs.jsx';
 import Contact from './components/contact/Contactjs.jsx';
 import Footer from './components/footer/Footerjs.jsx';
